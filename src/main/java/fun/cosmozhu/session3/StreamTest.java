4d7c390dd2b2597eb520f8d72d4a4b49265c3e26
cosmozhu <zhuchao1103@gmail.com>
2020-01-03 15:57:32 +0800

    add session3

--- src/main/java/fun/cosmozhu/session3/StreamTest.java
+++ src/main/java/fun/cosmozhu/session3/StreamTest.java
@@ -1,10 +1,13 @@
-package fun.cosmozhu.session2;
+package fun.cosmozhu.session3;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 /**
@@ -22,16 +25,17 @@ public class StreamTest {
 		//根据给定的元素创建一个数据流，元素必须是相同的类型，比如全部为String，或者全部为int。
 		DataStreamSource<String> ds = env.fromElements(words);
 		//Transformations
-		//对DataStream应用一个Map转换。对DataStream中的每一个元素都会调用MapFunction接口的具体实现类。map方法仅仅只返回一个元素，这个flatMap不同
-		DataStream<String> map = ds.map(new MapFunction<String, String>() {
+		//对DataStream应用一个flatMap转换。对DataStream中的每一个元素都会调用FlatMapFunction接口的具体实现类。flatMap方法可以返回任意个元素，当然也可以什么都不返回。
+		SingleOutputStreamOperator<String> flatMap = ds.flatMap(new FlatMapFunction<String, String>() {
 			@Override
-			public String map(String value) throws Exception {
-				return value.toUpperCase();
+			public void flatMap(String value, Collector<String> out) throws Exception {
+				out.collect(value);
+				out.collect(value.toUpperCase());
 			}
 		});
 		//sinks打印出信息
 		//给DataStream添加一个Sinks
-		map.addSink(new SinkFunction<String>() {
+		flatMap.addSink(new SinkFunction<String>() {
 			@Override
 			public void invoke(String value) throws Exception {
 				LOG.info(value);
